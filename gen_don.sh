74131ae2b7b44ba595d839e387af4c8c0e4f6c64
Vincent Delmas <vincent.delmas@hotmail.fr>
2019-07-30 17:36:58 -0400

    genere les fichier de config maillages pour lancer le code sur n gpus

---
+++ gen_don.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in `seq 0 $1`;do
+echo "&DATAMESH  ALEAT=1 , MAIL=2 , MTRI=1 , NNX=153 , NNY=8 , meshfile='${i}_Mille_Iles_mesh_743968_elts.txt' , 
+           elt_bound=0, numerot=1 , zcolumn=1,
+	   ncel=0,
+           elts=140,  911, /" >> "${i}_donnemai_mille_iles.txt"
+done
+
