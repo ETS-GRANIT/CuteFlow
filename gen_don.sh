60d17a5fdd64b8aee5b9284c44117cda86775b82
Vincent Delmas <vincent.delmas@hotmail.fr>
2019-09-17 01:48:40 -0400

    cleaning

--- gen_don.sh
+++ gen_don.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 for i in `seq 0 $1`;do
+cp "../../mesh/out_mesh.$i.txt" "${i}_Mille_Iles_mesh_743968_elts.txt"
+
 echo "&DATAMESH  ALEAT=1 , MAIL=2 , MTRI=1 , NNX=153 , NNY=8 , meshfile='${i}_Mille_Iles_mesh_743968_elts.txt' , 
            elt_bound=0, numerot=1 , zcolumn=1,
 	   ncel=0,
