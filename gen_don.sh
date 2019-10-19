ced12a2902392c7a4ba277d3c20d0488c6ecb912
Vincent Delmas <vincent.delmas@hotmail.fr>
2019-10-18 20:31:49 -0700

    marche pour les gros fichiers de maillages mais l'échange mémoire sur GPU est lent il doit y avoir une limite sur la taille maximale d'un message echangé sur GPU pour l'instant ça fait les échanges pour les mailes 1 par 1 du coup c'est très lent

--- gen_don.sh
+++ gen_don.sh
@@ -6,6 +6,6 @@ cp "../../mesh/out_mesh.$i.txt" "${i}_Mille_Iles_mesh_743968_elts.txt"
 echo "&DATAMESH  ALEAT=1 , MAIL=2 , MTRI=1 , NNX=153 , NNY=8 , meshfile='${i}_Mille_Iles_mesh_743968_elts.txt' , 
            elt_bound=0, numerot=1 , zcolumn=1,
 	   ncel=0,
-           elts=140,  911, /" >> "${i}_donneemai_mille_iles.txt"
+           elts=140,  911, /" > "${i}_donneemai_mille_iles.txt"
 done
 
