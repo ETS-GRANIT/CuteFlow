dfb93e11f0ebe97dc84521b5e6a2cdb288558096
Vincent Delmas <vincent.delmas@hotmail.fr>
2019-11-28 07:38:31 -0800

    adding gpu files

---
+++ gpu_config/16_gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH --mem=8000M
+#SBATCH --time=0-02:00
+#SBATCH --account=def-soulaima
+#SBATCH --output=outfile.out
+
+module load pgi/19.4 cuda/10.0.130 openmpi/3.1.2
+
+mpirun --mca pml ob1 --mca btl openib -n 16 sh -c './runfile > outfile.$OMPI_COMM_WORLD_RANK'
