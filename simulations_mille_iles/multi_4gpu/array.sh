26a91e6f1c7c34c727871349c01dd5d13817bd36
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-02-11 07:47:05 -0500

    array add

--- simulations_mille_iles/multi_4gpu/array.sh
+++ simulations_mille_iles/multi_4gpu/array.sh
@@ -3,10 +3,11 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:2
 #SBATCH --mem=8000M
-#SBATCH --time=0-06:00
+#SBATCH --time=0-10:00
 #SBATCH --account=def-soulaima
-#SBATCH --output=outfile.out
+#SBATCH --array=1-20
 
 module load pgi/19.4 cuda/10.0.130 openmpi/3.1.2
 
+cd multi_${SLURM_ARRAY_TASK_ID}
 mpirun --mca pml ob1 --mca btl openib -n 4 sh -c './runfile > outfile.$OMPI_COMM_WORLD_RANK'
