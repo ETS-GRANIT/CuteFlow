40987f2334f3a7ddf64416d37a4991bf442a4130
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-02-19 14:44:57 -0500

    multi_2 folder updated

---
+++ simulations_mille_iles/multi_2gpu/base_files/2_gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:2
+#SBATCH --mem=8000M
+#SBATCH --time=0-30:00
+#SBATCH --account=def-soulaima
+#SBATCH --output=outfile.out
+
+module load pgi/19.4 cuda/10.0.130 openmpi/3.1.2
+
+mpirun --mca pml ob1 --mca btl openib -n 2 sh -c './runfile > outfile.$OMPI_COMM_WORLD_RANK'
