62811a78276155bb729802c802f27f0acc5baefd
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-02-19 20:34:29 -0500

    ajout gen_res pour regrouper les solutions

--- simulations_mille_iles/multi_2gpu/array.sh
+++ simulations_mille_iles/multi_2gpu/array.sh
@@ -3,10 +3,9 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:2
 #SBATCH --mem=8000M
-#SBATCH --time=0-01:00
+#SBATCH --time=0-02:00
 #SBATCH --account=def-soulaima
 #SBATCH --array=1-20
-#SBATCH --output=multi_${SLURM_ARRAY_TASK_ID}/outfile.out
 
 module load pgi/19.4 cuda/10.0.130 openmpi/3.1.2
 
