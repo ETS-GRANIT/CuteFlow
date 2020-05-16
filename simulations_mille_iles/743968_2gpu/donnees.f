f2cc7ac5dbb2a06cef9146df16a9bd800f816e94
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-05-15 20:20:43 -0400

    fixed tolerence sec/mouille riad het zoka fonctionne sur 743968 elements

--- simulations_mille_iles/743968_2gpu/donnees.f
+++ simulations_mille_iles/743968_2gpu/donnees.f
@@ -2,7 +2,7 @@
 
   ! Données du terrain
   GP=9.81,
-  is_override_manning=1, override_manning=0.02200,               ! nombre de manning qui override les autes si is_...=1
+  is_override_manning=0, override_manning=0.02200,               ! nombre de manning qui override les autes si is_...=1
 
   ! Données du maillage
   meshfile='Mille_Iles_mesh_743968_elts.txt',                    ! Fichier de maillage
@@ -13,7 +13,7 @@
   eqlin_barrage=1,                                               ! 1 pour initialiser avec un barrage
   x1eqbar=274997.75521489076, x2eqbar=274756.1533974407,         ! Abscisses des deux points du barrage
   y1eqbar=5043578.489765059, y2eqbar=5043885.982987268,          ! Ordonnées des deux points du barrage
-  H_AMONT=32.0, U_AMONT=0, V_AMONT=0,                            ! Valeur de l'init du coté - de la normale au barrage
+  H_AMONT=30.0, U_AMONT=0, V_AMONT=0,                            ! Valeur de l'init du coté - de la normale au barrage
   H_AVAL =0.0, U_AVAL =0, V_AVAL =0,                             ! Valeur de l'init du coté + de la normale au barrage
 
   ! Initialisation avec un plan 
@@ -27,22 +27,22 @@
 
   ! Conditions aux limites
   inlet='inflow',                                                ! Type d'entrée : {inflow,transm}
-  debitglob=800.0,                                                 ! Débit aux entrées, séparer les débits pas des ,
+  debitglob=0.0,                                                 ! Débit aux entrées, séparer les débits pas des ,
   debit_var=0, loi_debitglob='Mille_iles_Qt.txt',                ! Loi de debit fonctionne uniquement avec 1 entrée
   H_sortie=0.0,                                                  ! Hauteur du niveau à la sortie du domaine
 
   ! Paramètres des schémas numériques
   IFLUX=2,                                                       ! 1 -> HLLC zoka, 2 -> HLLC Riadh
-  tolisec=1.0E-05,                                               ! Tolérence sec/mouillé pour HLLC zoka
+  tolisec=1.0E-03,                                               ! Tolérence sec/mouillé
   timedisc='euler',                                              ! Schéma en temps : {euler,second,runge}
   friction=1,                                                    ! 1 pour prendre en compte la friction
   fricimplic=1,                                                  ! 0 -> explicite, 1 -> I-dt/2*J, 2 -> I-dt*B
   is_dry_as_wall=0,                                              ! 1 pour mettre les mailles seches comme des murs
   local_time_step=0,                                             ! 1 pour utiliser le local time step
 
-  TS=100.0, CFL=0.6,                                              ! Temps maximal de simultion, nombre CFL
+  TS=20.0, CFL=0.9,                                              ! Temps maximal de simultion, nombre CFL
   tol_reg_perm=1.0E-15,                                          ! Tolérence relative entre debit entrée et débit sortie
-  freqaffich=10000,                                               ! Frequence de print dans outfile.[0-9]
+  freqaffich=100,                                               ! Frequence de print dans outfile.[0-9]
 
   ! Sauvegarde de la solution
   nbrjauges=0, jauges_snapshots=100,                             ! Nombre de jauges, nombre de snapshots
@@ -55,5 +55,5 @@
 
   solrestart=1, restart_snapshots=10,                            ! Sauvegarde en overwrite la solution pour restart
   solbasic=0, basic_snapshots=10,                                ! 1 pour sauvegarder h sur les noeud (pierre)
-  solvtk=1, vtk_snapshots=10,                                    ! 1 pour sauvegarder les fichiers vkt pour video
+  solvtk=1, vtk_snapshots=20,                                    ! 1 pour sauvegarder les fichiers vkt pour video
   sortie_finale_bluekenue=0/                                     ! Sauvergarde fichiers T3S à la fin
