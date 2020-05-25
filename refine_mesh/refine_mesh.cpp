e8598dd3d069f0d0897803aeea9f94486e9e2f9f
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-05-25 03:52:56 -0700

    ajout code pour raffinement

---
+++ refine_mesh/refine_mesh.cpp
@@ -0,0 +1,507 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <Eigen/Sparse>
+
+typedef Eigen::SparseMatrix<int> SpMat;
+
+using namespace std;
+
+void ecriture(bool multi_sortie, bool multi_entree, string fileName, vector<vector<double> > &nodes, vector<vector<double> > &elems, vector<int> &new_entreNodes, vector<int> &new_numEntreNodes, vector<int> &new_sortieNodes, vector<int> &new_numSortieNodes, vector<int> &new_wallNodes, int &nombreEntre, int &nombreSortie){
+
+  int i, nNodes, nElems;
+  nNodes = nodes.size();
+  nElems = elems.size();
+  ofstream out_mesh;
+  stringstream ss;
+  //ss << "out_mesh." << n << ".txt";
+  ss << "Refined_" << fileName;
+  out_mesh.open(ss.str());
+  ss.str("");
+  out_mesh << fixed ;
+  out_mesh.precision(4);
+  out_mesh << "Table de coordonnees" << endl;
+  out_mesh << nNodes << endl;
+
+  i=0;
+  while(i<nNodes){
+    out_mesh << (int) (nodes[i][0]) << " " << nodes[i][1]<< " " << nodes[i][2] << " " << nodes[i][3] <<" " << nodes[i][4] << endl ;
+    i++; 
+  }
+
+  out_mesh << "Table de connectivités" << endl;
+  out_mesh << nElems << endl;
+  i=0;
+  while(i<nElems){
+    out_mesh << (int) (elems[i][0]) << " " << (int) (elems[i][1]) << " " << (int) (elems[i][2]) << " " << (int) (elems[i][3]) << " " << elems[i][4] << endl ;
+    i++; 
+  }
+
+  out_mesh << "Noeuds d'entre" << endl;
+  i=0;
+  if(multi_entree){
+    out_mesh << new_entreNodes.size() << " " << nombreEntre << endl;
+    while(i<new_entreNodes.size()){
+      out_mesh << new_entreNodes[i] <<  " " << new_numEntreNodes[i] << endl ;
+      i++; 
+    }
+  }
+  else{
+    out_mesh << new_entreNodes.size() << endl;
+    while(i<new_entreNodes.size()){
+      out_mesh << new_entreNodes[i] << endl ;
+      i++; 
+    }
+  }
+
+  out_mesh << "Noeuds de sortie" << endl;
+  i=0;
+  if(multi_sortie){
+    out_mesh << new_sortieNodes.size() <<  " " << nombreSortie << endl;
+    while(i<new_sortieNodes.size()){
+      out_mesh << new_sortieNodes[i] << " " << new_numSortieNodes[i] << endl ;
+      i++; 
+    }
+  }
+  else{
+    out_mesh << new_sortieNodes.size() << endl;
+    while(i<new_sortieNodes.size()){
+      out_mesh << new_sortieNodes[i] << endl ;
+      i++; 
+    }
+  }
+
+  out_mesh << "Noeuds de murs" << endl;
+  out_mesh << new_wallNodes.size() << endl;
+  i=0;
+  while(i<new_wallNodes.size()){
+    out_mesh << new_wallNodes[i] << endl ;
+    i++; 
+  }
+
+  out_mesh.close();
+}
+/* refine(nodes, elems, entreNodes, numEntreNodes, sortieNodes, numSortieNodes, wallNodes, boundTag, new_nodes, new_elems, new_entreNodes, new_numEntreNodes, new_sortieNodes, new_numSortieNodes, new_wallNodes); */
+void refine(vector<vector<double> > &nodes, vector<vector<double> > &elems, vector<int> &entreNodes, vector<int> &numEntreNodes, vector<int> &sortieNodes, vector<int> &numSortieNodes, vector<int> &wallNodes, vector<vector<int> > &boundTag, vector<vector<double> > &new_nodes, vector<vector<double> > &new_elems, vector<int> &new_entreNodes, vector<int> &new_numEntreNodes, vector<int> &new_sortieNodes, vector<int> &new_numSortieNodes, vector<int> &new_wallNodes){
+
+  int ns = nodes.size();
+  int s1, s2, s3;
+  double s1x, s1y, s1z, s1m;
+  double s12x, s12y, s12z, s12m;
+  double s2x, s2y, s2z, s2m;
+  double s23x, s23y, s23z, s23m;
+  double s3x, s3y, s3z, s3m;
+  double s31x, s31y, s31z, s31m;
+  int ns1, ns12, ns2, ns23, ns3, ns31;
+  //vector<vector<int> > is_created(nodes.size(), vector<int> (nodes.size(), -1));
+  //SpMat is_created(nodes.size(), nodes.size());
+  unordered_map<int, int> is_created;
+  vector<double> v(5);
+
+  for(int i=0;i<elems.size();i++){
+    //Ajout des nouveaux noeuds
+    s1 = elems[i][1]-1;
+    s2 = elems[i][2]-1;
+    s3 = elems[i][3]-1;
+
+    if(is_created.find(s1*ns+s1)==is_created.end()){
+      s1x = nodes[s1][1];
+      s1y = nodes[s1][2];
+      s1z = nodes[s1][3];
+      s1m = nodes[s1][4];
+
+      v[0] = new_nodes.size()+1;
+      ns1=v[0]-1;
+      is_created[s1*ns+s1] = ns1+1;
+      v[1] = s1x;
+      v[2] = s1y;
+      v[3] = s1z;
+      v[4] = s1m;
+      new_nodes.push_back(v);
+    }
+    else{
+      ns1=is_created[s1*ns+s1]-1;
+      s1x = new_nodes[ns1][1];
+      s1y = new_nodes[ns1][2];
+      s1z = new_nodes[ns1][3];
+      s1m = new_nodes[ns1][4];
+    }
+
+    if(is_created.find(s2*ns+s2)==is_created.end()){
+      s2x = nodes[s2][1];
+      s2y = nodes[s2][2];
+      s2z = nodes[s2][3];
+      s2m = nodes[s2][4];
+
+      v[0] = new_nodes.size()+1;
+      ns2=v[0]-1;
+      is_created[s2*ns+s2] = ns2+1;
+      v[1] = s2x;
+      v[2] = s2y;
+      v[3] = s2z;
+      v[4] = s2m;
+      new_nodes.push_back(v);
+    }
+    else{
+      ns2=is_created[s2*ns+s2]-1;
+      s2x = new_nodes[ns2][1];
+      s2y = new_nodes[ns2][2];
+      s2z = new_nodes[ns2][3];
+      s2m = new_nodes[ns2][4];
+    }
+
+    if(is_created.find(s3*ns+s3)==is_created.end()){
+      s3x = nodes[s3][1];
+      s3y = nodes[s3][2];
+      s3z = nodes[s3][3];
+      s3m = nodes[s3][4];
+
+      v[0] = new_nodes.size()+1;
+      ns3=v[0]-1;
+      is_created[s3*ns+s3] = ns3+1;
+      v[1] = s3x;
+      v[2] = s3y;
+      v[3] = s3z;
+      v[4] = s3m;
+      new_nodes.push_back(v);
+    }
+    else{
+      ns3=is_created[s3*ns+s3]-1;
+      s3x = new_nodes[ns3][1];
+      s3y = new_nodes[ns3][2];
+      s3z = new_nodes[ns3][3];
+      s3m = new_nodes[ns3][4];
+    }
+
+    if(is_created.find(s1*ns+s2)==is_created.end()){
+      s12x = (s1x+s2x)/2.;
+      s12y = (s1y+s2y)/2.;
+      s12z = (s1z+s2z)/2.;
+      s12m = (s1m+s2m)/2.;
+
+      v[0] = new_nodes.size()+1;
+      ns12=v[0]-1;
+      is_created[s1*ns+s2] = ns12+1;
+      is_created[s2*ns+s1] = ns12+1;
+      v[1] = s12x;
+      v[2] = s12y;
+      v[3] = s12z;
+      v[4] = s12m;
+      new_nodes.push_back(v);
+    }
+    else{
+      ns12=is_created[s1*ns+s2]-1;
+      s12x = new_nodes[ns12][1];
+      s12y = new_nodes[ns12][2];
+      s12z = new_nodes[ns12][3];
+      s12m = new_nodes[ns12][4];
+    }
+
+    if(is_created.find(s2*ns+s3)==is_created.end()){
+      s23x = (s3x+s2x)/2.;
+      s23y = (s3y+s2y)/2.;
+      s23z = (s3z+s2z)/2.;
+      s23m = (s3m+s2m)/2.;
+
+      v[0] = new_nodes.size()+1;
+      ns23=v[0]-1;
+      is_created[s2*ns+s3] = ns23+1;
+      is_created[s3*ns+s2] = ns23+1;
+      v[1] = s23x;
+      v[2] = s23y;
+      v[3] = s23z;
+      v[4] = s23m;
+      new_nodes.push_back(v);
+    }
+    else{
+      ns23=is_created[s2*ns+s3]-1;
+      s23x = new_nodes[ns23][1];
+      s23y = new_nodes[ns23][2];
+      s23z = new_nodes[ns23][3];
+      s23m = new_nodes[ns23][4];
+    }
+
+    if(is_created.find(s3*ns+s1)==is_created.end()){
+      s31x = (s1x+s3x)/2.;
+      s31y = (s1y+s3y)/2.;
+      s31z = (s1z+s3z)/2.;
+      s31m = (s1m+s3m)/2.;
+
+      v[0] = new_nodes.size()+1;
+      ns31=v[0]-1;
+      is_created[s1*ns+s3] = ns31+1;
+      is_created[s3*ns+s1] = ns31+1;
+      v[1] = s31x;
+      v[2] = s31y;
+      v[3] = s31z;
+      v[4] = s31m;
+      new_nodes.push_back(v);
+    }
+    else{
+      ns31=is_created[s3*ns+s1]-1;
+      s31x = new_nodes[ns31][1];
+      s31y = new_nodes[ns31][2];
+      s31z = new_nodes[ns31][3];
+      s31m = new_nodes[ns31][4];
+    }
+
+    /* cout << "elems " << i << endl; */
+
+    //Ajout des nouveaux éléments
+    v[0] = new_elems.size()+1;
+    v[1] = ns1+1;
+    v[2] = ns12+1;
+    v[3] = ns31+1;
+    v[4] = elems[i][4];
+    new_elems.push_back(v);
+
+    v[0] = new_elems.size()+1;
+    v[1] = ns12+1;
+    v[2] = ns2+1;
+    v[3] = ns23+1;
+    v[4] = elems[i][4];
+    new_elems.push_back(v);
+
+    v[0] = new_elems.size()+1;
+    v[1] = ns23+1;
+    v[2] = ns3+1;
+    v[3] = ns31+1;
+    v[4] = elems[i][4];
+    new_elems.push_back(v);
+
+    v[0] = new_elems.size()+1;
+    v[1] = ns12+1;
+    v[2] = ns23+1;
+    v[3] = ns31+1;
+    v[4] = elems[i][4];
+    new_elems.push_back(v);
+
+    //Gestion des entree sorties mur
+    if(boundTag[s1][0] == -1){
+      new_entreNodes.push_back(ns1+1);
+      new_numEntreNodes.push_back(boundTag[s1][1]);
+    }
+    else if(boundTag[s1][0] == -2){
+      new_sortieNodes.push_back(ns1+1);
+      new_numSortieNodes.push_back(boundTag[s1][1]);
+    }
+    else if(boundTag[s1][0] == -3){
+      new_wallNodes.push_back(ns1+1);
+    }
+
+    if(boundTag[s2][0] == -1){
+      new_entreNodes.push_back(ns2+1);
+      new_numEntreNodes.push_back(boundTag[s2][1]);
+    }
+    else if(boundTag[s2][0] == -2){
+      new_sortieNodes.push_back(ns2+1);
+      new_numSortieNodes.push_back(boundTag[s2][1]);
+    }
+    else if(boundTag[s2][0] == -3){
+      new_wallNodes.push_back(ns2+1);
+    }
+
+    if(boundTag[s3][0] == -1){
+      new_entreNodes.push_back(ns3+1);
+      new_numEntreNodes.push_back(boundTag[s3][1]);
+    }
+    else if(boundTag[s3][0] == -2){
+      new_sortieNodes.push_back(ns3+1);
+      new_numSortieNodes.push_back(boundTag[s3][1]);
+    }
+    else if(boundTag[s3][0] == -3){
+      new_wallNodes.push_back(ns3+1);
+    }
+
+    if(boundTag[s1][0] == -1 and boundTag[s2][0] == -1){
+      new_entreNodes.push_back(ns12+1);
+      new_numEntreNodes.push_back(boundTag[s1][1]);
+    }
+    if(boundTag[s1][0] == -2 and boundTag[s2][0] == -2){
+      new_sortieNodes.push_back(ns12+1);
+      new_numSortieNodes.push_back(boundTag[s1][1]);
+    }
+    if(boundTag[s1][0] == -3 and boundTag[s2][0] == -3){
+      new_wallNodes.push_back(ns12+1);
+    }
+
+    if(boundTag[s2][0] == -1 and boundTag[s3][0] == -1){
+      new_entreNodes.push_back(ns23+1);
+      new_numEntreNodes.push_back(boundTag[s2][1]);
+    }
+    if(boundTag[s2][0] == -2 and boundTag[s3][0] == -2){
+      new_sortieNodes.push_back(ns23+1);
+      new_numSortieNodes.push_back(boundTag[s2][1]);
+    }
+    if(boundTag[s2][0] == -3 and boundTag[s3][0] == -3){
+      new_wallNodes.push_back(ns23+1);
+    }
+
+    if(boundTag[s3][0] == -1 and boundTag[s1][0] == -1){
+      new_entreNodes.push_back(ns31+1);
+      new_numEntreNodes.push_back(boundTag[s3][1]);
+    }
+    if(boundTag[s3][0] == -2 and boundTag[s1][0] == -2){
+      new_sortieNodes.push_back(ns31+1);
+      new_numSortieNodes.push_back(boundTag[s3][1]);
+    }
+    if(boundTag[s3][0] == -3 and boundTag[s1][0] == -3){
+      new_wallNodes.push_back(ns31+1);
+    }
+
+  }
+}
+
+void lecture(bool multi_entree, bool multi_sortie, ifstream &mesh, vector<vector<double> > &nodes, vector<vector<double> > &elems, vector<int> &entreNodes, vector<int> &numEntreNodes, vector<int> &sortieNodes, vector<int> &numSortieNodes, vector<int> &wallNodes, int &nombreEntre, int &nombreSortie, vector<vector<int> > &boundTag){
+
+  int i, nNodes, nElems, nSortie, nEntre, nWall;
+  string str;
+  vector<double> v(5);
+
+  i=0;
+  getline(mesh, str);
+  mesh >> nNodes;
+  boundTag.resize(nNodes, vector<int> (2,0));
+  cout.precision(4);
+  //cout << fixed;
+  while(i<nNodes){
+    v[0] = 0.;
+    v[1] = 0.;
+    v[2] = 0.;
+    v[3] = 0.;
+    v[4] = 0.;
+    mesh >> v[0] >> v[1] >> v[2] >> v[3] >> v[4] ;
+    nodes.push_back(v);
+    i++;
+  }
+  i=0;
+  getline(mesh, str);
+  getline(mesh, str);
+  mesh >> nElems;
+  while(i<nElems){
+    elems.push_back(v);
+    mesh >> elems[i][0] >> elems[i][1] >> elems[i][2] >> elems[i][3] >> elems[i][4];
+    i++;
+  }
+  getline(mesh, str);
+  getline(mesh, str);
+  if(multi_entree){
+    mesh >> nEntre >> nombreEntre;
+  }
+  else{
+    mesh >> nEntre;
+    nombreEntre=1;
+  }
+  cout << "Lecture noeuds entree " << nEntre << endl;
+  i=0;
+  int dum;
+  vector<int> dum_v(2);
+  if(multi_entree){
+    while(i<nEntre){
+      mesh >> dum_v[0] >> dum_v[1];
+      boundTag[dum_v[0]][0] = -1; //tag -1 pour entree
+      boundTag[dum_v[0]][1] = dum_v[1]; //tag -1 pour entree
+      entreNodes.push_back(dum_v[0]);
+      numEntreNodes.push_back(dum_v[1]);
+      i++;
+    }
+  }
+  else{
+    while(i<nEntre){
+      mesh >> dum_v[0];
+      dum_v[1] = 1; //unique entrée numéro 1
+      boundTag[dum_v[0]][0] = -1; //tag -1 pour entree
+      boundTag[dum_v[0]][1] = dum_v[1]; //tag -1 pour entree
+      entreNodes.push_back(dum_v[0]);
+      numEntreNodes.push_back(dum_v[1]);
+      i++;
+    }
+  }
+  getline(mesh, str);
+  getline(mesh, str);
+  if(multi_sortie){
+    mesh >> nSortie >> nombreSortie;
+  }
+  else{
+    mesh >> nSortie;
+    nombreSortie=1;
+  }
+  cout << "Lecture noeuds sortie " << nSortie << endl;
+  i=0;
+  if(multi_sortie){
+    while(i<nSortie){
+      mesh >> dum_v[0] >> dum_v[1];
+      boundTag[dum_v[0]][0] = -2; //tag -1 pour sortie
+      boundTag[dum_v[0]][1] = dum_v[1]; //tag -1 pour entree
+      sortieNodes.push_back(dum_v[0]);
+      numSortieNodes.push_back(dum_v[1]);
+      i++;
+    }
+  }
+  else{
+    while(i<nSortie){
+      mesh >> dum_v[0];
+      dum_v[1] = 1; //unique sortie numero 1
+      boundTag[dum_v[0]][0] = -2; //tag -1 pour sortie
+      boundTag[dum_v[0]][1] = dum_v[1]; //tag -1 pour entree
+      sortieNodes.push_back(dum_v[0]);
+      numSortieNodes.push_back(dum_v[1]);
+      i++;
+    }
+  }
+  getline(mesh, str);
+  getline(mesh, str);
+  mesh >> nWall;
+  cout << "Lecture noeuds wall " << nWall << endl;
+  i=0;
+  while(i<nWall){
+    mesh >> dum;
+    wallNodes.push_back(dum);
+    boundTag[dum][0] = -3; //tag -1 pour mur
+    i++;
+  }
+
+  cout << "Lecture elem finie, nNodes:" << nNodes << " nElems:" << nElems << endl;
+}
+
+int main(int argc, char* argv[]){
+
+  ifstream mesh;
+  string fileName;
+  bool multi_entree=0;
+  bool multi_sortie=0;
+
+  if(argc < 4){
+    cout << "Utilisation : ./split_mesh fichier_de_maillage multi_entrees multi_sorties" << endl;
+    cout << "multi_entree=1 uniquement si le maillage a découper posséde plusieurs entrées" << endl;
+    cout << "multi_sortie=1 uniquement si le maillage a découper posséde plusieurs sorties" << endl;
+    return(0);
+  }
+  else{
+    fileName = argv[1];
+    mesh.open(argv[1]);
+    multi_entree = atoi(argv[2]);
+    multi_sortie = atoi(argv[3]);
+  }
+
+  vector<int> entreNodes, sortieNodes, wallNodes, numEntreNodes, numSortieNodes;
+  vector<int> new_entreNodes, new_sortieNodes, new_wallNodes, new_numEntreNodes, new_numSortieNodes;
+  vector<vector<int> > boundTag;
+  vector<vector<double> > nodes, elems, new_nodes, new_elems;
+
+  int n, i, nNodes, nElems, nombreEntre, nombreSortie;
+
+  cout << fixed;
+
+  //void lecture(bool multi_entree, bool multi_sortie, ifstream &mesh, vector<vector<double> > &nodes, vector<vector<double> > &elems, vector<int> &entreNodes, vector<int> &numEntreNodes, vector<int> &sortieNodes, vector<int> &numSortieNodes, vector<int> &wallNodes, int &nombreEntre, int &nombreSortie, vector<int> &boundTag){
+  lecture(multi_entree, multi_sortie, mesh, nodes, elems, entreNodes, numEntreNodes, sortieNodes, numSortieNodes, wallNodes, nombreEntre, nombreSortie, boundTag);
+
+  refine(nodes, elems, entreNodes, numEntreNodes, sortieNodes, numSortieNodes, wallNodes, boundTag, new_nodes, new_elems, new_entreNodes, new_numEntreNodes, new_sortieNodes, new_numSortieNodes, new_wallNodes);
+
+
+  ecriture(multi_sortie, multi_entree, fileName, new_nodes, new_elems, new_entreNodes, new_numEntreNodes, new_sortieNodes, new_numSortieNodes, new_wallNodes, nombreEntre, nombreSortie);
+}
