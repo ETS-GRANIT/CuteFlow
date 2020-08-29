dfc1dcf0bb5e75fbb425759c9b323c3d39b023e1
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-08-28 22:13:43 -0700

    Ajout des scripts pour lancement server paraview

---
+++ scripts/load_para_cpu.sh
@@ -0,0 +1,1 @@
+module --force purge;module load nixpkgs/16.09  intel/2018.3  openmpi/3.1.2  paraview-offscreen; srun pvserver --mesa --force-offscreen-rendering
---
+++ scripts/request_visu.sh
@@ -0,0 +1,1 @@
+salloc --time=$1:00:0 --ntasks=16 --mem-per-cpu=12000M --account=def-soulaima
