80d6bd8e201aeba5961cc5f94a4cb12a02c25007
Vincent Delmas <vincent.delmas@hotmail.fr>
2020-08-30 18:32:52 -0700

    Changement du nom solbasic vers solsimple, ajout warning si le pas de temps est plus grand que 10000 et changement du nom des fichiers solution pour Paraview de FV-Para* vers solution-Para pour pouvoir supprimer tous les fichiers résultats en faisant rù *solution*

--- scripts/gen_donnees.sh
+++ scripts/gen_donnees.sh
@@ -14,7 +14,7 @@ echo "&DONNEES_NAMELIST
   x1eqbar=274997.75521489076, x2eqbar=274756.1533974407,         ! Abscisses des deux points du barrage
   y1eqbar=5043578.489765059, y2eqbar=5043885.982987268,          ! Ordonnées des deux points du barrage
   H_AMONT=$2, U_AMONT=0, V_AMONT=0,                            ! Valeur de l'init du coté - de la normale au barrage
-  H_AVAL =$3, U_AVAL =0, V_AVAL =0,                             ! Valeur de l'init du coté + de la normale au barrage
+  H_AVAL =$3, U_AVAL =0, V_AVAL =0,                            ! Valeur de l'init du coté + de la normale au barrage
 
   ! Initialisation avec un plan 
   plan=0                                                         ! 1 pour initialiser avec un plan
@@ -27,24 +27,25 @@ echo "&DONNEES_NAMELIST
 
   ! Conditions aux limites
   inlet='inflow',                                                ! Type d'entrée : {inflow,transm}
-  debitglob=$1,                                                 ! Débit aux entrées, séparer les débits pas des ,
+  debitglob=$1,                                               ! Débit aux entrées, séparer les débits pas des ,
   debit_var=0, loi_debitglob='Mille_iles_Qt.txt',                ! Loi de debit fonctionne uniquement avec 1 entrée
-  H_sortie=$3,                                                  ! Hauteur du niveau à la sortie du domaine
+  H_sortie=$3,                                                 ! Hauteur du niveau à la sortie du domaine
 
   ! Paramètres des schémas numériques
   IFLUX=2,                                                       ! 1 -> HLLC zoka, 2 -> HLLC Riadh
-  tolisec=1.0E-05,                                               ! Tolérence sec/mouillé pour HLLC zoka
+  tolisec=1.0E-06,                                               ! Tolérence sec/mouillé
   timedisc='euler',                                              ! Schéma en temps : {euler,second,runge}
   friction=1,                                                    ! 1 pour prendre en compte la friction
   fricimplic=1,                                                  ! 0 -> explicite, 1 -> I-dt/2*J, 2 -> I-dt*B
   is_dry_as_wall=0,                                              ! 1 pour mettre les mailles seches comme des murs
   local_time_step=0,                                             ! 1 pour utiliser le local time step
 
-  TS=100.0, CFL=0.9,                                              ! Temps maximal de simultion, nombre CFL
+  TS=300.0, CFL=0.9,                                             ! Temps maximal de simultion, nombre CFL
   tol_reg_perm=1.0E-15,                                          ! Tolérence relative entre debit entrée et débit sortie
   freqaffich=5000,                                               ! Frequence de print dans outfile.[0-9]
 
   ! Sauvegarde de la solution
+  merged_solution=0,                                             ! Ecriture de la solution recombinées (lent)
   nbrjauges=0, jauges_snapshots=100,                             ! Nombre de jauges, nombre de snapshots
   xjauges = 272494.31, 274767.53, 279066.02,                     ! Abscisses des jauges
   yjauges = 5042222.41, 5046515.26, 5050380.38,                  ! Ordonnées des jauges
@@ -53,8 +54,8 @@ echo "&DONNEES_NAMELIST
   coupe_a    = -0.7946,    -2.6754,    -1.4439                   ! Coeficient a de ax+b=y
   coupe_b    =  5.2585e+06, 5.7768e+06, 5.4397e+06               ! Coeficient b de ax+b=y
 
-  solrestart=1, restart_snapshots=2,                            ! Sauvegarde en overwrite la solution pour restart
-  solbasic=0, basic_snapshots=10,                                ! 1 pour sauvegarder h sur les noeud (pierre)
+  solrestart=0, restart_snapshots=10,                            ! Sauvegarde en overwrite la solution pour restart
+  solsimple=0, simple_snapshots=10,                              ! 1 noeuds, 2 éléments, 3 noeuds+elements
   solvtk=1, vtk_snapshots=10,                                    ! 1 pour sauvegarder les fichiers vkt pour video
   sortie_finale_bluekenue=0/                                     ! Sauvergarde fichiers T3S à la fin
 " > donnees.f
