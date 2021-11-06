21842346f59bf925bb251aeed8be269601b78ced
Vincent Delmas <vincent.delmas@hotmail.fr>
2021-11-06 03:17:50 -0400

    updated script to launch paraview server

--- scripts/load_para_cpu.sh
+++ scripts/load_para_cpu.sh
@@ -1,1 +1,1 @@
-module --force purge;module load nixpkgs/16.09  intel/2018.3  openmpi/3.1.2  paraview-offscreen; srun pvserver --mesa --force-offscreen-rendering
+module --force purge;module load nixpkgs/16.09  intel/2018.3  openmpi/3.1.4 paraview-offscreen/5.5.2; srun pvserver --mesa --force-offscreen-rendering
