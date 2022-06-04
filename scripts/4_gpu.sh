fd6d95eeae11010d6cbb7fd80c8f20ee3708531d
Vincent Delmas <vincent.delmas@hotmail.fr>
2022-06-04 10:53:53 -0400

    changements des scripts de lancement automatique de Cuteflow

--- scripts/4_gpu.sh
+++ scripts/4_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:p100:2
 #SBATCH --mem=8000M
@@ -7,4 +7,4 @@
 #SBATCH --account=def-soulaima
 #SBATCH --output=outfile.out
 
-mpirun --mca pml ob1 -n 2 sh -c './cuteflow > outfile.$OMPI_COMM_WORLD_RANK'
+mpirun --mca pml ob1 -n 4 sh -c './cuteflow > outfile.$OMPI_COMM_WORLD_RANK'
