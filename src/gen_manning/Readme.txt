3315bf20d79738cdfab1322fc35b2e343672950d
Igor Gildas Metcheka  Kengne <igor81@gra-login1.graham.sharcnet>
2024-02-19 14:10:31 -0500

    nouveau fichier gen_manning.py pour assigner les valeurs de manning et nouveau fichier Readme.txt

--- src/gen_manning/Readme.txt
+++ src/gen_manning/Readme.txt
@@ -5,6 +5,7 @@ virtualenv ENV #juste la première fois
 source ENV/bin/activate #Pour entrer dans l'ENV
 pip install --no-index --upgrade pip #juste la première fois
 pip install numpy scipy matplotlib pyCGNS #juste la première fois
+python3 upgrade_bib.py # Pour mettre à jour les bibliothèques installées
 deactivate #Pour sortir de l'ENV
 
 #Pour lancer le code
--- src/gen_manning/gen_manning.py
+++ src/gen_manning/gen_manning.py
@@ -12,45 +12,32 @@ import matplotlib.pyplot as plt
 from sys import argv
 from scipy import interpolate
 
-#Points d'interpolation
-xi = np.array([292780.9505926187,293630.8979144815,294267.7279965635,294444.64941000944,296858.89325867774,298436.47734251834,299908.4017718763,301459.6199652275,302612.16334976535,303647.07659505267,304700.1783846825,301949.4462004238,301593.7315237351,303627.65504811704,304690.98004778684,307186.98859399126,308627.93489410076,295480.16815198876,296466.61433280207,297257.6731355314,300284.27222212753,303645.2641893357,308728.47998152586,310125.2328527561,307228.5928085854], dtype=np.float64)
-yi = np.array([5050402.622491601,5051821.805653197,5053450.872929014,5054341.85610797,5056338.961016703,5057662.523767,5058279.15286715,5059671.05266955,5061255.28925471,5062362.791906215,5062870.9946411215,5039516.269662259,5042964.397613532,5046911.33968053,5052617.894845575,5057199.225578135,5062057.024485555,5061404.626547684,5061426.705538602,5061125.683636666,5062304.60494403,5062463.9404099425,5052470.15903622,5066696.108016082,5067603.006517198], dtype=np.float64)
-n = xi.size
-zi = np.array([0.037,0.037,0.037,0.04,0.05,0.05,0.05,0.02,0.01,0.01,0.01,0.03,0.03,0.03,0.03,0.02,0.02,0.05,0.05,0.01,0.01,0.01,0.02,0.01,0.01], dtype=np.float64)
+# Définition du nombre de manning dominant
+average_manning = float(0.022)
 
-#Construction d'une regression polynomiale
-M = np.zeros((n, 21), dtype=np.float64)
-for i in range(n):
-    M[i,0] = 1.
-    M[i,1] = xi[i]
-    M[i,2] = yi[i]
-    M[i,3] = xi[i]*yi[i]
-    M[i,4] = xi[i]**2
-    M[i,5] = yi[i]**2
-    M[i,6] = xi[i]*yi[i]**2
-    M[i,7] = yi[i]*xi[i]**2
-    M[i,8] = xi[i]**3
-    M[i,9] = yi[i]**3
-    M[i,10] = (xi[i]**1)*(yi[i]**3)
-    M[i,11] = (xi[i]**2)*(yi[i]**2)
-    M[i,12] = (xi[i]**3)*(yi[i]**1)
-    M[i,13] = (xi[i]**4)
-    M[i,14] = (yi[i]**4)
-    M[i,15] = (xi[i]**1)*(yi[i]**4)
-    M[i,16] = (xi[i]**2)*(yi[i]**3)
-    M[i,17] = (xi[i]**3)*(yi[i]**2)
-    M[i,18] = (xi[i]**4)*(yi[i]**1)
-    M[i,19] = (xi[i]**5)
-    M[i,20] = (yi[i]**5)
-Q, R = np.linalg.qr(M)
-c = np.linalg.solve(np.matmul(np.transpose(R),R), np.dot(np.transpose(M),zi))
-print(np.dot(M,c)) #Verrification
+# Valeurs du coefficient de manning aux cellules à modifier
+cell_index = np.array([], dtype=np.int64)
+manning_value = np.array([],dtype=np.float64)
 
-#Fonction de regression
-def fman(x,y):
-    z = c[0]+c[1]*x+c[2]*y+c[3]*x*y+c[4]*x**2+c[5]*y**2+c[6]*x*y**2+c[7]*y*x**2+c[8]*x**3+c[9]*y**3+c[10]*x*y**3+c[11]*(x**2)*(y**2)+c[12]*(x**3)*y+c[13]*x**4+c[14]*y**4+c[15]*x*y**4+c[16]*(x**2)*(y**3)+c[17]*(x**3)*(y**2)+c[18]*(x**4)*y+c[19]*x**5+c[20]*y**5
-    return(z)
+# Lire les données à partir du fichier text
+with open('./Entree_gen_manning.txt', 'r') as file:
+    lines = file.readlines()
 
+# Extraire les valeurs de Cells IDs et Manning_values
+cell_ids_line = lines[1]
+manning_values_line = lines[3]
+
+# Supprimer les caractères de nouvelle ligne
+cell_ids_line = cell_ids_line.strip()
+manning_values_line = manning_values_line.strip()
+
+# Convertir les lignes en listes d'entiers et de nombres à virgule flottante
+cell_ids = [int(value) for value in cell_ids_line.split(',')]
+manning_values = [float(value) for value in manning_values_line.split(',')]
+
+# Remplir les tableaux
+cell_index = np.array(cell_ids, dtype=np.int64)
+manning_value = np.array(manning_values, dtype=np.float64)
 
 #Lecture du fichier CGNS
 (tree,links,paths)=CGNS.MAP.load(argv[1]) # Load le fichier CGNS donné en argument
@@ -65,35 +52,22 @@ nnodes = int(len(x))
 nelems = int(len(conn)/3)
 mannings = np.zeros(int(len(conn)/3), dtype=np.float64)
 nout = 0
-for i in range(nelems):
-    s1 = conn[3*i]-1
-    s2 = conn[3*i+1]-1
-    s3 = conn[3*i+2]-1
-    cx = (x[s1]+x[s2]+x[s3])/3.
-    cy = (y[s1]+y[s2]+y[s3])/3.
 
-    #Moyenne pondérée des nombres de Manning sur les points les plus proches
-    d = np.zeros((len(xi)), dtype=np.float64)
-    for k in range(len(xi)):
-        d[k] = sqrt((xi[k]-cx)**2+(yi[k]-cy)**2)
-    ii = np.argsort(d)
-    md = 0.
-    mm = 0.
-    for k in range(0,20):
-        if(d[ii[k]] < 1000.) : d[ii[k]] = 1000
-        mm = mm + zi[ii[k]]/d[ii[k]]
-        md = md + (1./d[ii[k]])
-    mannings[i] = mm/md
+for i in range (nelems):
+    mannings[i]=average_manning
 
-    # Utilisation de la fonction de regression
-    # mannings[i] = fman(cx,cy)
+for j in range (len(cell_index)):
+    mannings[cell_index[j]]=manning_value[j]
 
-    #Correction potentielle des nombres de mannings s'ils sont trop grands ou trop petits
-    if(mannings[i] > 0.05 or mannings[i] < 0.): nout+=1
-    if(mannings[i] > 0.05) : mannings[i] = 0.05
-    if(mannings[i] < 0.) : mannings[i] = 0.01
+ #Correction potentielle des nombres de mannings s'ils sont trop grands ou trop petits
+    if(mannings[i] > 0.05 or mannings[i] < 0.): 
+        nout+=1
+    if(mannings[i] > 0.05) : 
+        mannings[i] = 0.05
+    if(mannings[i] < 0.) : 
+        mannings[i] = 0.01
 
-print(nout,"values out of range 0.00 0.05")
+print(nout,"values out of range [0.00 ; 0.05]")
 man = CGL.newUserDefinedData(node, "Manning")
 CGL.newGridLocation(man, CK.CellCenter_s)
 CGL.newDataArray(man, '{DataArray}', value=mannings)
